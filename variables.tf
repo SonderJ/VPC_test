50e1580331bc99f90f2206c9ba8775dc1c7c994d
SonderJ <34524346+SonderJ@users.noreply.github.com>
2020-01-23 12:00:29 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "ssh_key" {
   default = "jsonder"
-  description = "Name of the SSH key that you uploaded to your IBM Cloud account. You enter the value for your variable when you create your workspace in IBM Cloud Schematics. Use the following CLI command to  retrieve all uploaded keys in your current region: ibmcloud is keys
-"
+  description = "Name of the SSH key that you uploaded to your IBM Cloud account. You enter the value for your variable when you create your workspace in IBM Cloud Schematics. Use the following CLI command to  retrieve all uploaded keys in your current region: ibmcloud is keys"
   }
 
 variable "resource_group" {
-  default = ""
+  default = "default"
   description = "Group resources to organize and assign access to the group. Available for all resources managed by IBM Cloud Identity and Access Management. Use the following CLI command to  retrieve all resource groups: ibmcloud resource groups"
-}
+  }
 
 variable "vpc_generation" {
   default = "1"
